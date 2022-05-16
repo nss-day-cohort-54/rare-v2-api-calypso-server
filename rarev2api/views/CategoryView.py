990860a3bd78fd47d7ee8bed687ae5012aca58b5
Katie Dawkins <katiemdawkins@gmail.com>
2022-05-16 14:37:32 -0500

    setup

---
+++ rarev2api/views/CategoryView.py
@@ -0,0 +1,49 @@
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers, status
+
+from rarev2api.models.category import Category
+
+class CategoryView(ViewSet):
+    """Rare V2 Category View"""
+    
+    def retrieve(self, request, pk):
+        """Handle GET requests for single category
+        
+        Returns: 
+            Response -- JSON serialized category
+        """
+        try:
+            category = Category.objects.get(pk=pk)
+            serializer = CategorySerializer(category)
+            return Response(serializer.data)
+        except Category.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+
+    def list(self, request):
+        """Handle GET requests to get all categories
+        
+        Returns:
+            Response -- JSON serialized list of categories
+        """
+        categories = Category.objects.all()
+        serializer = CategorySerializer(categories, many=True)
+        return Response(serializer.data)
+        
+    def create(self, request):
+        serializer = CreateCategorySerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('id', 'label')  
+
+class CreateCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('id', 'label')
