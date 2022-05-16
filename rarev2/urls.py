a59fd77a595dce90c4c254950c2b93d6cebe2f65
Wil Wardrop <wwardrop@gmail.com>
2022-05-16 14:15:39 -0500

    store away

--- rarev2/urls.py
+++ rarev2/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from rarev2api.views.auth import login_user, register_user
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login', login_user),
+    path('register', register_user)
 ]
